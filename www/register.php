0a5709f907e182817ffe9ab336f1f2225ca08282
Taiki <taiki@rakshata.com>
2014-12-05 04:10:24 +0100

    More specific symbols, funny commit message incoming

--- www/register.php
+++ www/register.php
@@ -34,7 +34,7 @@
     }
     else
     {
-        echo "GOT AWAY LOL" . (isset($_SESSION['_registerToken']) ? "1" : "0")  . " - " . ($_SESSION['_registerToken'] !== 0 ? "1" : "0") . " - " . ($_REQUEST['_registerToken'] === $_SESSION['_registerToken'] ? "1" : "0")  . " - " . $_REQUEST['_registerToken']  . " - " . $_SESSION['_registerToken'];
+        echo "GOT AWAY LOL" . (isset($_REQUEST['_registerToken']) ? "1" : "0")  . " - " . $_REQUEST['_registerToken']  . " - " . $_SESSION['_registerToken'];
     }
     require_once("theme/header.php");
     $_SESSION['_registerToken'] = rand(1, 0xffffffff);
