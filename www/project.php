ed48d4e992b6df69a11d633029a0ac6cf46d921e
p1rox <p1rox@p1rox.fr>
2014-12-05 01:01:57 +0100

    Add project page

---
+++ www/project.php
@@ -0,0 +1,35 @@
+<?php require_once("theme/header.php"); ?>
+<div class="container" role="main">
+    <div class="jumbotron">
+        <h1>My super project</h1>
+        <p>Here a super description of my project !</p>
+    </div>
+</div>
+<div class="container">
+	<div class="col-md-6">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Info sur le projet</h3>
+			</div>
+			<div class="panel-body">
+				<p><strong>Lieu : </strong> Hello</p>
+				<p><strong>Cout du projet : </strong>42</p>
+				<p><strong>Bla : </strong>bla bla ...</p>
+			</div>
+  		</div>
+  	</div>
+  	<div class="col-md-6">
+  		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Other stuff here</h3>
+			</div>
+			<div class="panel-body">
+				<p class="text-center">
+					<img src="http://lorempicsum.com/futurama/350/200/1" alt="..." class="img-rounded">
+				</p>
+			</div>
+  		</div>
+  	</div>
+</div>
+<?php require_once("theme/footer.php"); ?>
+
