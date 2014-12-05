4000dad92d55d7743e670ecc453aea6ba321f750
Ybalrid <ybalrid@cecmu.org>
2014-12-05 03:49:41 +0100

    add a butifull 404 image

--- www/404.php
+++ www/404.php
@@ -3,6 +3,7 @@
     <div class="jumbotron">
         <h1>404 <strong>Not found !</strong> </h1>
         <p>La page que vous cherchez n'existe pas ou plus.</p>
+        <img src="theme/resources/404.png" style="width:100%;">
     </div>
 </div>
 <?php require_once("theme/footer.php"); ?>
